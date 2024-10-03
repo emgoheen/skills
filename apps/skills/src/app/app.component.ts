162a63b4dd9e5e7458b37c93dc0dd44dbf308ebf
Emily Tracy <emgoheen@yahoo.com>
2024-10-03 15:05:28 -0700

    Incorporated Angular Material

--- apps/skills/src/app/app.component.ts
+++ apps/skills/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbar, MatButton],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
