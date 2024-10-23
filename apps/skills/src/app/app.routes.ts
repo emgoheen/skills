377bc131e7d2c98a14783a6f4f2ecde778cfc110
Emily Tracy <emgoheen@yahoo.com>
2024-10-23 12:48:42 -0700

    Added some initial theming and built out side nav component

--- apps/skills/src/app/app.routes.ts
+++ apps/skills/src/app/app.routes.ts
@@ -1,21 +1,24 @@
 import { Route } from '@angular/router';
 
 export enum AppRoutes {
-    CANDIDATES_SIDE_NAV_ROUTE = 'candidates-side-nav'
+  CANDIDATES_SIDE_NAV_ROUTE = 'skills',
 }
 export const appRoutes: Route[] = [
-    {
-        path: '',
-        redirectTo: AppRoutes.CANDIDATES_SIDE_NAV_ROUTE,
-        pathMatch: 'full'    
-    },
-    {
-        path: AppRoutes.CANDIDATES_SIDE_NAV_ROUTE,
-        loadComponent: () => import('@skills/candidates/feature').then((mod) => mod.CandidateSideNavComponent)
-    },
-    {
-        path: '**',
-        pathMatch: 'full',
-        redirectTo: AppRoutes.CANDIDATES_SIDE_NAV_ROUTE
-    }
+  {
+    path: '',
+    redirectTo: AppRoutes.CANDIDATES_SIDE_NAV_ROUTE,
+    pathMatch: 'full',
+  },
+  {
+    path: AppRoutes.CANDIDATES_SIDE_NAV_ROUTE,
+    loadComponent: () =>
+      import('@skills/candidates/feature').then(
+        (mod) => mod.CandidateSideNavComponent,
+      ),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: AppRoutes.CANDIDATES_SIDE_NAV_ROUTE,
+  },
 ];
--- libs/candidates/feature/src/lib/candidates-feature/candidate-side-nav.component.ts
+++ libs/candidates/feature/src/lib/candidates-feature/candidate-side-nav.component.ts
@@ -3,14 +3,24 @@ import { CommonModule } from '@angular/common';
 import { CandidateStore } from './candidate.store';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'lib-candidate-side-nav',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    RouterModule,
+    MatButtonModule,
+  ],
   templateUrl: './candidate-side-nav.component.html',
   styleUrl: './candidate-side-nav.component.scss',
   providers: [CandidateStore],
